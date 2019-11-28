9feb2d962ae2edbdd419a1247fe9a1597f57a04b
barth2501 <lancelot2501>
2019-11-28 17:12:22 +0100

    currency model finally pushed

--- back/bitcoincoin/models/currency.py
+++ back/bitcoincoin/models/currency.py
@@ -1,13 +1,19 @@
 from peewee import *
 from playhouse.shortcuts import model_to_dict
-
+from base_model import BaseModel
 
 class CurrencyRates(BaseModel):
     id = PrimaryKeyField()
-    currency_id = ForeignKeyField()
+    currency_id = ForeignKeyField(Currencies)
     datetime = DateTimeField()
     value = FloatField()
 
-class LastCurrency(BaseModel):
+    def save(self):
+        BaseModel.save(self)
+        currency = Currencies.get(Currencies.id == self.currency_id)
+        currency.last_value = self.value
+
+class Currencies(BaseModel):
     id = PrimaryKeyField()
     name = TextField()
+    last_value = FloatField()
