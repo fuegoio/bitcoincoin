b843081c23457c7b50d2212d5dcf1837a75a5324
GabrielHerzhaft <gabriel.herzhaft@laposte.net>
2019-12-02 16:18:50 +0100

    Remove post request for user creation (will use auth api)

--- back/bitcoincoin/api/users.py
+++ back/bitcoincoin/api/users.py
@@ -2,19 +2,14 @@ from flask import request
 from flask_restful import Resource
 
 # TODO: import from managers all useful functions
-    # search_users(query)
-    # create_user(username, password, email)
-    # get_user_by_id(user_id)
-    # delete_user(user_id)
+# search_users(query)
+# get_user_by_id(user_id)
+# delete_user(user_id)
 
 
 class Users(Resource):
     def get(self):
-        return search_users(request.args['query'])
-
-    def post(self):
-        data = request.json
-        return create_user(data['username'], data['password'], data['email'])
+        return search_users(request.args["query"])
 
 
 class User(Resource):
