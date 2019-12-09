46030e617f7285863e4a653f75efb046c35b88a5
GabrielHerzhaft <gabriel.herzhaft@laposte.net>
2019-12-09 15:29:54 +0100

    Add transactions api calls

--- back/bitcoincoin/api/__init__.py
+++ back/bitcoincoin/api/__init__.py
@@ -12,6 +12,7 @@ from .users import (
     UserWalletCurrency,
 )
 from .currencies import Currencies, Currency, CurrencyLastRate, CurrencyRates
+from .transactions import Transactions
 
 api_bp = Blueprint("api", __name__)
 api = Api(api_bp)
@@ -43,5 +44,7 @@ def register_api(app):
     api.add_resource(Currency, "/currencies/<currency_id:int>")
     api.add_resource(CurrencyRates, "/currencies/<currency_id:int>/rates")
     api.add_resource(CurrencyLastRate, "/currencies/<currency_id:int>/rates/last")
+
+    api.add_resource(Transactions, "/transactions")
 
     app.register_blueprint(api_bp, url_prefix="/api/v1")
