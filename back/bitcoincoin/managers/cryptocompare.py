a7adaabe1c9c3d6a382401d6e0effc8b6ceb501c
barth2501 <lancelot2501>
2019-12-09 15:55:52 +0100

    get_historic function

--- back/bitcoincoin/managers/cryptocompare.py
+++ back/bitcoincoin/managers/cryptocompare.py
@@ -7,22 +7,17 @@ headers = {'authorization': 'Apikey {}'.format('7bdf4764d02940ee49fa5198c51d2cfb
 def update_currency():
     url_all = 'https://min-api.cryptocompare.com/data/blockchain/list'
     response_all = requests.get(url_all, headers=headers)
-    print('ok')
     for cur in response_all.json()['Data']:
-        url_unique = 'https://min-api.cryptocompare.com/data/v2/histominute?fsym=AIT&tsym=GBP&limit=1'
-        print('ok')
+        url_unique = 'https://min-api.cryptocompare.com/data/v2/histominute?fsym={}&tsym=USD&limit=1'.format(cur)
         try:
             currency = Currency.get_or_none(symbol=cur)
             response_unique = requests.get(url_unique, headers=headers)
             value = response_unique.json()['Data']['Data'][-1]
             data = {'symbol' : cur, 'last_value':value['close'], 'name':cur}
-            print(cur)
             if currency is None:
-                print('not exist')
                 currency = Currency.create(**data)
             else:
                 update_model_from_dict(currency,data)
-            print(currency)
             data = {'currency':currency.id, 'datetime':datetime.datetime.fromtimestamp(value['time']), 'value':value['close']}
             currency_rate = CurrencyRate(**data)
             currency_rate.save()
@@ -30,5 +25,22 @@ def update_currency():
             print(e)
             continue
     return True
+
+def get_historic(symbol,nb_days=730):
+    url = 'https://min-api.cryptocompare.com/data/v2/histoday?fsym={}&tsym=USD&limit={}'.format(symbol,nb_days)
+    response = requests.get(url,headers=headers)
+    currency = Currency.get_or_none(symbol=symbol)
+    if currency:
+        for day in response.json()['Data']:
+            try:
+                data = {'currency':currency.id, 'datetime':datetime.datetime.fromtimestamp(day['time']), 'value':day['close']}
+                currency_rate = CurrencyRate(**data)
+                currency_rate.save()
+            except Exception as e:
+                print(e)
+                continue
+    return True
+
+
 
 update_currency()
