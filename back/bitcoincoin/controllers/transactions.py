d72b3a85edae28572b3b85d645ea4e2c7caf0a52
GabrielHerzhaft <gabriel.herzhaft@laposte.net>
2020-01-13 14:15:30 +0100

    Add wallet check when creating a transaction

--- back/bitcoincoin/controllers/transactions.py
+++ back/bitcoincoin/controllers/transactions.py
@@ -1,5 +1,6 @@
 from bitcoincoin.models.transaction import Transaction
 from .currencies import get_currency_last_rate
+from .users import get_user_cash_flow, get_user_wallet
 
 
 def search_transactions(filters: dict):
@@ -27,5 +28,18 @@ def search_transactions(filters: dict):
 
 def create_transaction(user_id: int, currency_id: int, quantity: int, is_sell: bool):
     value = get_currency_last_rate(currency_id) * quantity
-    # TODO: check if value is available in user wallet
-    return Transaction.create(user=user_id, currency=currency_id, quantity=quantity, value=value, is_sell=is_sell)
+    if is_sell:
+        currency_available = get_user_wallet(user_id, currency_id)
+        if currency_available < quantity:
+            raise Exception("Not enough money available to make this transaction")
+    else:
+        dollars_available = get_user_cash_flow(user_id)
+        if dollars_available < value:
+            raise Exception("Not enough dollars available to make this transactions")
+    return Transaction.create(
+        user=user_id,
+        currency=currency_id,
+        quantity=quantity,
+        value=value,
+        is_sell=is_sell,
+    )
