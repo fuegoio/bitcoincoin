f0783d94eb6e572c3f90ddbfd136eb247ef8cf90
GabrielHerzhaft <gabriel.herzhaft@laposte.net>
2020-01-13 16:03:08 +0100

    Use black formatting

--- back/bitcoincoin/controllers/currencies.py
+++ back/bitcoincoin/controllers/currencies.py
@@ -9,25 +9,32 @@
 from datetime import date
 from bitcoincoin.models.currency import Currency, CurrencyRate
 
-def search_currencies(query:dict):
+
+def search_currencies(query: dict):
     currencies = Currency.select()
-    if 'symbol' in query:
-        currencies = currencies.where(Currency.symbol==str(query['symbol']))
-    if 'name' in query:
-        currencies = currencies.where(Currency.name==str(query['name']))
+    if "symbol" in query:
+        currencies = currencies.where(Currency.symbol == str(query["symbol"]))
+    if "name" in query:
+        currencies = currencies.where(Currency.name == str(query["name"]))
     return [cur.get_small_data() for cur in currencies]
 
-def create_currency(name:str, symbol:str, last_value:float, provider:str):
-    return Currency.create(name=name, symbol=symbol, last_value=last_value, provider=provider)
 
-def get_currency_by_id(currency_id:int):
+def create_currency(name: str, symbol: str, last_value: float, provider: str):
+    return Currency.create(
+        name=name, symbol=symbol, last_value=last_value, provider=provider
+    )
+
+
+def get_currency_by_id(currency_id: int):
     return Currency.get(id=currency_id).get_small_data()
 
-def delete_currency(currency_id:int):
+
+def delete_currency(currency_id: int):
     return Currency.get(id=currency_id).delete_instance()
 
-def get_currency_rates_history(currency_id:int, query:dict):
-    history = CurrencyRate.select().where(CurrencyRate.currency==currency_id)
+
+def get_currency_rates_history(currency_id: int, query: dict):
+    history = CurrencyRate.select().where(CurrencyRate.currency == currency_id)
     # if 'date_debut' in query:
     #     try:
     #         date_debut = date(query['date_debut'])
@@ -42,10 +49,12 @@ def get_currency_rates_history(currency_id:int, query:dict):
     #     history = history.where(CurrencyRate.datetime <= query['date_fin'])
     return [hist.get_small_data() for hist in history]
 
-def create_currency_rate(currency_id:int, value:float):
-    #TODO: add datetime
+
+def create_currency_rate(currency_id: int, value: float):
+    # TODO: add datetime
     return CurrencyRate.create(currency=currency_id, value=value)
 
-def get_currency_last_rate(currency_id:int):
+
+def get_currency_last_rate(currency_id: int):
     currency = get_currency_by_id(currency_id)
-    return currency['last_value']
+    return currency["last_value"]
