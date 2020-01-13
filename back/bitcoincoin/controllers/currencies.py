7beb18daa212e19d4bef1f7d7152c0f2f29fa838
GabrielHerzhaft <gabriel.herzhaft@laposte.net>
2020-01-13 15:52:00 +0100

    Move errors checking to api files, use our own errors

--- back/bitcoincoin/controllers/currencies.py
+++ back/bitcoincoin/controllers/currencies.py
@@ -12,96 +12,39 @@ from bitcoincoin.models.currency import Currency, CurrencyRate
 def search_currencies(query:dict):
     currencies = Currency.select()
     if 'symbol' in query:
-        try:
-            symbol = str(query['symbol'])
-        except:
-            raise TypeError('symbol must be str')
-        currencies = currencies.where(Currency.symbol==symbol)
+        currencies = currencies.where(Currency.symbol==str(query['symbol']))
     if 'name' in query:
-        try:
-            name = str(query['name'])
-        except TypeError:
-            print('currency_name must be str')
-        currencies = currencies.where(Currency.name==name)
+        currencies = currencies.where(Currency.name==str(query['name']))
     return [cur.get_small_data() for cur in currencies]
 
 def create_currency(name:str, symbol:str, last_value:float, provider:str):
-    try:
-        name = str(name)
-    except:
-        raise TypeError('name must be string')
-    try:
-        symbol = str(symbol)
-    except:
-        raise TypeError('symbol must be string')
-    try:
-        last_value = float(last_value)
-        assert last_value > 0
-    except:
-        raise TypeError('last_value must be float')
-    try:
-        provider = str(provider)
-        assert provider in ['cryptocompare','coincap']
-    except:
-        raise TypeError('provider is not known')  
-    data = {'symbol': symbol, 'last_value': last_value, 'name': name, 'provider':provider}
-    return Currency.create(**data)
+    return Currency.create(name=name, symbol=symbol, last_value=last_value, provider=provider)
 
 def get_currency_by_id(currency_id:int):
-    try:
-        currency_id = int(currency_id)
-        assert currency_id > 0
-    except:
-        raise TypeError('currency_id must be int and positive')
-    return Currency.get_or_none(id=currency_id).get_small_data()
+    return Currency.get(id=currency_id).get_small_data()
 
 def delete_currency(currency_id:int):
-    try:
-        currency_id = int(currency_id)
-        assert currency_id > 0
-    except:
-        raise TypeError('currency_id must be int and positive')
-    return Currency.get_or_none(id=currency_id).delete_instance()
+    return Currency.get(id=currency_id).delete_instance()
 
 def get_currency_rates_history(currency_id:int, query:dict):
-    try:
-        currency_id = int(currency_id)
-        assert currency_id > 0
-    except:
-        raise TypeError('currency_id must be int and positive')
     history = CurrencyRate.select().where(CurrencyRate.currency==currency_id)
-    if 'date_debut' in query:
-        try:
-            date_debut = date(query['date_debut'])
-        except:
-            raise TypeError('date debut is not well given')
-        history = history.where(CurrencyRate.datetime >= query['date_debut'])
-    if 'date_fin' in query:
-        try:
-            date_debut = date(query['date_debut'])
-        except:
-            raise TypeError('date debut is not well given')
-        history = history.where(CurrencyRate.datetime <= query['date_fin'])
+    # if 'date_debut' in query:
+    #     try:
+    #         date_debut = date(query['date_debut'])
+    #     except:
+    #         raise TypeError('date debut is not well given')
+    #     history = history.where(CurrencyRate.datetime >= query['date_debut'])
+    # if 'date_fin' in query:
+    #     try:
+    #         date_debut = date(query['date_debut'])
+    #     except:
+    #         raise TypeError('date debut is not well given')
+    #     history = history.where(CurrencyRate.datetime <= query['date_fin'])
     return [hist.get_small_data() for hist in history]
 
-def create_currency_rate(currency_id:int, datetime:date, value:float):
-    try:
-        currency_id = int(currency_id)
-        assert currency_id > 0
-    except:
-        raise TypeError('currency_id must be int and positive')
-    try:
-        datetime = #TODO
-    except:
-        raise TypeError('date in not well given')
-    try:
-        value = float(value)
-        assert value>0
-    except:
-        raise TypeError('value is not int or not positive')
-    data = {'currency': currency_id, 'datetime': datetime,
-                    'value': value}
-    return CurrencyRate.create(**data)
+def create_currency_rate(currency_id:int, value:float):
+    #TODO: add datetime
+    return CurrencyRate.create(currency=currency_id, value=value)
 
 def get_currency_last_rate(currency_id:int):
     currency = get_currency_by_id(currency_id)
--- back/bitcoincoin/web/api/currencies.py
+++ back/bitcoincoin/web/api/currencies.py
@@ -11,6 +11,8 @@ from flask_restful import Resource
 # create_currency_rate(currency_id, value)
 
 from bitcoincoin.controllers.currencies import *
+from bitcoincoin.errors.bad_resource import *
+
 
 class Currencies(Resource):
     def get(self):
@@ -19,22 +21,76 @@ class Currencies(Resource):
     # is it useful ? or we create currencies only in backend ?
     def post(self):
         data = request.json
-        return create_currency(name=data["name"], symbol=data["symbol"], last_value=data['last_value'], provider=data['provider'])
+        try:
+            name = str(data["name"])
+            assert len(name) > 0
+        except:
+            raise EmptyStringError(data["name"])
+        try:
+            symbol = str(data["symbol"])
+            assert len(name) > 0
+        except:
+            raise EmptyStringError(data["symbol"])
+        try:
+            last_value = float(data["last_value"])
+            assert last_value > 0
+        except:
+            raise BadCurrencyValueError(data["last_value"])
+        try:
+            provider = str(data["provider"])
+            assert provider in ["cryptocompare", "coincap"]
+        except:
+            raise BadCurrencyProviderError(data["provider"])
+        return create_currency(
+            name=name, symbol=symbol, last_value=last_value, provider=provider
+        )
+
 
 class Currency(Resource):
     def get(self, currency_id):
+        try:
+            currency_id = int(currency_id)
+            assert currency_id > 0
+        except:
+            raise BadIdError(currency_id)
         return get_currency_by_id(currency_id)
 
     def delete(self, currency_id):
+        try:
+            currency_id = int(currency_id)
+            assert currency_id > 0
+        except:
+            raise BadIdError(currency_id)
         return delete_currency(currency_id)
+
 
 class CurrencyRates(Resource):
     def get(self, currency_id):
+        try:
+            currency_id = int(currency_id)
+            assert currency_id > 0
+        except:
+            raise BadIdError(currency_id)
         return get_currency_rates_history(currency_id, request.args)
 
     def post(self, currency_id):
         data = request.json
-        return create_currency_rate(currency_id=currency_id, datetime=data['datetime'], value=data['value'])
+        try:
+            currency_id = int(currency_id)
+            assert currency_id > 0
+        except:
+            raise BadIdError(currency_id)
+        # try:
+        #     datetime = #TODO
+        # except:
+        #     raise TypeError('date in not well given')
+        try:
+            value = float(data["value"])
+            assert value > 0
+        except:
+            raise BadCurrencyValueError(value)
+        return create_currency_rate(currency_id=currency_id, value=value)
+
 
 class CurrencyLastRate(Resource):
     def get(self, currency_id):
