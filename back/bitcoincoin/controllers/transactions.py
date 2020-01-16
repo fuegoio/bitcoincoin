37f6fa36ec0fe9b26c3000c68019c9865569edd5
GabrielHerzhaft <gabriel.herzhaft@laposte.net>
2020-01-16 11:14:50 +0100

    Fix copy-paste from currency to transaction, use black formatting

--- back/bitcoincoin/controllers/transactions.py
+++ back/bitcoincoin/controllers/transactions.py
@@ -17,9 +17,13 @@ def search_transactions(filters: dict):
             Transaction.currency == filters["currency"]
         )
     if "from_date" in filters:
-        history = history.where(CurrencyRate.datetime >= filters["from_date"])
+        transactions_found = transactions_found.where(
+            Transaction.datetime >= filters["from_date"]
+        )
     if "to_date" in filters:
-        history = history.where(CurrencyRate.datetime <= filters["to_date"])
+        transactions_found = transactions_found.where(
+            Transaction.datetime <= filters["to_date"]
+        )
     return [transaction.get_small_data() for transaction in transactions_found]
 
 
@@ -28,17 +32,33 @@ def create_transaction(user_id: int, currency_id: int, quantity: int, is_sale: b
         if is_sale:
             user_wallet_list = get_user_wallet(user_id, currency_id)
             if len(user_wallet_list) > 0 and user_wallet_list[0]["volume"] >= quantity:
-                if not Wallet.update(volume=Wallet.volume-quantity).where(Wallet.user == user_id, Wallet.currency == currency_id).execute():
-                    raise Exception("Problem while updating user's wallet for currency, transaction cancelled")
+                if (
+                    not Wallet.update(volume=Wallet.volume - quantity)
+                    .where(Wallet.user == user_id, Wallet.currency == currency_id)
+                    .execute()
+                ):
+                    raise Exception(
+                        "Problem while updating user's wallet for currency, transaction cancelled"
+                    )
             else:
-                raise Exception("Not enough money available for this currency to make this transaction")
+                raise Exception(
+                    "Not enough money available for this currency to make this transaction"
+                )
         else:
             value = get_currency_last_rate(currency_id) * quantity
             dollars_available = get_user_cash_flow(user_id)
             if dollars_available < value:
-                raise Exception("Not enough dollars available to make this transactions")
-            if not User.update(cash_flow=User.cash_flow-value).where(User.id == user_id).execute():
-                raise Exception("Problem while updating user's cash flow, transaction cancelled")
+                raise Exception(
+                    "Not enough dollars available to make this transactions"
+                )
+            if (
+                not User.update(cash_flow=User.cash_flow - value)
+                .where(User.id == user_id)
+                .execute()
+            ):
+                raise Exception(
+                    "Problem while updating user's cash flow, transaction cancelled"
+                )
         return Transaction.create(
             user=user_id,
             currency=currency_id,
