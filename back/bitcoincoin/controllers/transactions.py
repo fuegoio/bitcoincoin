2266b18bae42c8397c95776b1cee9092fc31b49d
GabrielHerzhaft <gabriel.herzhaft@laposte.net>
2020-01-16 10:46:26 +0100

    Update create transaction controller
    Change user's cash flow or wallet depending on type of transaction

--- back/bitcoincoin/controllers/transactions.py
+++ back/bitcoincoin/controllers/transactions.py
@@ -1,4 +1,7 @@
 from bitcoincoin.models.transaction import Transaction
+from bitcoincoin.models.user import User
+from bitcoincoin.models.wallet import Wallet
+from bitcoincoin.core import db
 from .currencies import get_currency_last_rate
 from .users import get_user_cash_flow, get_user_wallet
 
@@ -17,24 +20,29 @@ def search_transactions(filters: dict):
         history = history.where(CurrencyRate.datetime >= filters["from_date"])
     if "to_date" in filters:
         history = history.where(CurrencyRate.datetime <= filters["to_date"])
-
     return [transaction.get_small_data() for transaction in transactions_found]
 
 
 def create_transaction(user_id: int, currency_id: int, quantity: int, is_sell: bool):
-    value = get_currency_last_rate(currency_id) * quantity
-    if is_sell:
-        currency_available = get_user_wallet(user_id, currency_id)
-        if currency_available < quantity:
-            raise Exception("Not enough money available to make this transaction")
-    else:
-        dollars_available = get_user_cash_flow(user_id)
-        if dollars_available < value:
-            raise Exception("Not enough dollars available to make this transactions")
-    return Transaction.create(
-        user=user_id,
-        currency=currency_id,
-        quantity=quantity,
-        value=value,
-        is_sell=is_sell,
-    )
+    with db.transaction():
+        if is_sell:
+            user_wallet_list = get_user_wallet(user_id, currency_id)
+            if len(user_wallet_list) > 0 and user_wallet_list[0]["volume"] >= quantity:
+                if not Wallet.update(volume=Wallet.volume-quantity).where(Wallet.user == user_id, Wallet.currency == currency_id).execute():
+                    raise Exception("Problem while updating user's wallet for currency, transaction cancelled")
+            else:
+                raise Exception("Not enough money available for this currency to make this transaction")
+        else:
+            value = get_currency_last_rate(currency_id) * quantity
+            dollars_available = get_user_cash_flow(user_id)
+            if dollars_available < value:
+                raise Exception("Not enough dollars available to make this transactions")
+            if not User.update(cash_flow=User.cash_flow-value).where(User.id == user_id).execute():
+                raise Exception("Problem while updating user's cash flow, transaction cancelled")
+        return Transaction.create(
+            user=user_id,
+            currency=currency_id,
+            quantity=quantity,
+            value=value,
+            is_sell=is_sell,
+        )
